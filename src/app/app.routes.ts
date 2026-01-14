3704fe40d838b59da516cd908f7c9f40fdfee0f4
anand nerurkar <anand.nerurkar@gmail.com>
2026-01-14 20:18:06 +0530

    1st commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Chapters } from './features/chapters';
+import { Shlokas } from './features/shlokas';
+import { TodaySandesh } from './features/today-sandesh';
+export const routes: Routes = [
+  { path: '', component: Chapters },
+  { path: 'chapter/:chapter', component: Shlokas },
+  { path: 'today-sandesh', component: TodaySandesh }
+];
 
-export const routes: Routes = [];
+
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Chapters } from './features/chapters';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, Chapters, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('bhagwat-gita-mobile-app-ui');
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  private cdr = inject(ChangeDetectorRef);
+
+  chapters: any[] = [];
+  error = '';
+
+  ngOnInit(): void {
+    console.log('APP INIT – ngOnInit called');
+    console.log('CALLING API:', 'https://kshna-svc-100157816972.asia-south1.run.app/api/gita/chapters');
+
+    this.http
+      .get<any[]>('https://kshna-svc-100157816972.asia-south1.run.app/api/gita/chapters')
+      .subscribe({
+        next: (data) => {
+          console.log('API SUCCESS – total chapters:', data.length);
+          this.chapters = data;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          console.error('API FAILED', err);
+          this.error = 'API failed';
+          this.cdr.detectChanges();
+        }
+      });
+  }
 }
